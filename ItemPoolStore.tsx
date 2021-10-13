4376085044651063d74b963a0ff6a5d442f7ad8e
Christian Danheimer Furedal <christian.furedal@swace.se>
2021-10-13 14:46:54 +0200

    Create ItemPoolStore.tsx

---
+++ ItemPoolStore.tsx
@@ -0,0 +1,65 @@
+import { observable } from 'mobx'
+import { IApiItem } from '~libs/models'
+import { isArray, merge } from '~libs/utils'
+
+export class ItemPoolStore {
+  @observable
+  itemPool: { [key: string]: IApiItem[] } = {}
+
+  findBy = <T extends IApiItem>(key: string, value: string, poolKey: string) => {
+    return this.getPool(poolKey).find((item) => item[key] === value) as T
+  }
+
+  /**
+   * Recursively get all items from the pool.
+   * Each item that doesn't already exist in the pool will be added.
+   * If the item already exist, it will instead be updated.
+   * The items in the pool will be returned
+   *
+   * All items returned will be an observable mobx object
+   */
+  getArrayFromPool = <T extends IApiItem>(items: T[]) => {
+    return items.map((item) => this.getFromPool(item)) as T[]
+  }
+
+  /**
+   * Recursively get item from the pool.
+   * If the item didn't exist, it will be added to the pool.
+   * If the the item already existed, it will be updated with the new data.
+   *
+   * The item returned will be an observable mobx object
+   */
+  getFromPool = <T extends IApiItem>(item: T) => {
+    if (!item.id || !item.__name) {
+      return item
+    }
+
+    Object.keys(item).forEach((key) => {
+      if (isArray(item[key])) {
+        item[key] = this.getArrayFromPool(item[key])
+      } else if (typeof item[key] === 'object') {
+        item[key] = this.getFromPool(item[key])
+      }
+    })
+
+    const pool = this.getPool(item.__name)
+    let index = pool.findIndex((i) => i.id === item.id)
+    if (index === -1) {
+      index = pool.push(item) - 1
+    } else {
+      merge(item, pool[index])
+    }
+
+    return pool[index] as T
+  }
+
+  /**
+   * Get pool by key. A pool will be created for the given key if it doesn't already exist.
+   */
+  getPool = (key: string) => {
+    if (!this.itemPool[key]) {
+      this.itemPool[key] = observable([])
+    }
+    return this.itemPool[key]
+  }
+}
